367b9f6a187e8387dd6ff6c33972344417769ebe
Matias Nahuel Heredia <matias@informaticos.com>
2014-06-07 18:56:48 -0300

    crear Administracion Netbook

---
+++ dom/src/main/java/dom/Netbook/Netbook.java
@@ -0,0 +1,91 @@
+package dom.Netbook;
+
+
+import java.util.Date;
+
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+
+
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ObjectType;
+import org.apache.isis.applib.annotation.Title;
+
+@javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE)
+@javax.jdo.annotations.Version(
+        strategy=VersionStrategy.VERSION_NUMBER, 
+        column="version")
+@ObjectType("NETBOOK")
+public class Netbook {
+	
+	private String idNetbook;
+	private String modelo;
+	private String numeroDeSerie;
+	private String numeroLicenciaWindows;
+	private Date fechaDeExpiracion;
+	private String direccionMac;
+	private String situacionDeNetbook;
+	
+	
+	@javax.jdo.annotations.Column(allowsNull="false")
+	public String getDireccionMac() {
+		return direccionMac;
+	}
+	public void setDireccionMac(String direccionMac) {
+		this.direccionMac = direccionMac;
+	}
+	@javax.jdo.annotations.Column(allowsNull="false")
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	@javax.jdo.annotations.Column(allowsNull="false")
+	public String getNumeroDeSerie() {
+		return numeroDeSerie;
+	}
+	public void setNumeroDeSerie(String numeroDeSerie) {
+		this.numeroDeSerie = numeroDeSerie;
+	}
+	@javax.jdo.annotations.Column(allowsNull="false")
+	public String getNumeroLicenciaWindows() {
+		return numeroLicenciaWindows;
+	}
+	public void setNumeroLicenciaWindows(String numeroLicenciaWindows) {
+		this.numeroLicenciaWindows = numeroLicenciaWindows;
+	}
+	@javax.jdo.annotations.Column(allowsNull="true")
+	public Date getFechaDeExpiracion() {
+		return fechaDeExpiracion;
+	}
+	public void setFechaDeExpiracion(Date fechaDeExpiracion) {
+		this.fechaDeExpiracion = fechaDeExpiracion;
+	}
+	
+	@javax.jdo.annotations.Column(allowsNull="false")
+	public String getSituacionDeNetbook() {
+		return situacionDeNetbook;
+	}
+	public void setSituacionDeNetbook(String situacionDeNetbook) {
+		this.situacionDeNetbook = situacionDeNetbook;
+	}
+	@javax.jdo.annotations.Column(allowsNull="false")
+    @Title(sequence="1")
+    @MemberOrder(sequence="1")
+	public String getIdNetbook() {
+		return idNetbook;
+	}
+	public void setIdNetbook(String idNetbook) {
+		this.idNetbook = idNetbook;
+	}
+	
+	@javax.inject.Inject
+    @SuppressWarnings("unused")
+    private DomainObjectContainer container;
+	
+}
---
+++ dom/src/main/java/repo/Netbook/RepositorioNetbook.java
@@ -0,0 +1,55 @@
+package repo.Netbook;
+
+
+import java.util.Date;
+
+
+
+
+import org.apache.isis.applib.AbstractFactoryAndRepository;
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Optional;
+
+import dom.Netbook.Netbook;
+
+@Named("Administrar Netbook")
+public class RepositorioNetbook extends AbstractFactoryAndRepository {
+
+
+	public String getId() {
+        return "Netbook";
+    }
+
+    public String iconName() {
+        return "SimpleObject";
+    }
+	
+	public Netbook ingresarNetbook(@Named("id de Netbook")final String idNetbook ,
+	@Named("Modelo")final String modelo,
+	@Named("Numero De Serie")final String numeroDeSerie,
+	@Named("Numero De Licencia de Windows")final String numeroLicenciaWindows,
+	@Named("Fecha de Expiracion") @Optional final Date fechaDeExpiracion,
+	@Named("Direccion Mac")final String direccionMac)
+	{
+		final Netbook netbook = container.newTransientInstance(Netbook.class);
+	    netbook.setFechaDeExpiracion(fechaDeExpiracion);
+	    netbook.setIdNetbook(idNetbook);
+	    netbook.setDireccionMac(direccionMac);
+	    
+	    netbook.setModelo(modelo);
+	    netbook.setNumeroDeSerie(numeroDeSerie);
+	    netbook.setNumeroLicenciaWindows(numeroLicenciaWindows);
+	    netbook.setSituacionDeNetbook("Entregada");
+	    
+	    
+	    container.persistIfNotAlready(netbook);
+	    
+		return netbook;
+	}
+	
+	@javax.inject.Inject 
+    DomainObjectContainer container;
+		
+	
+}
