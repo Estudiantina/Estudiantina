b79ec266b254fb2432592f29b5feda2640e02047
Matias Nahuel Heredia <matias@informaticos.com>
2014-06-21 04:38:22 -0300

    subir impresion HardCodeada

--- dom/src/main/java/dom/SolicitudDeServicioTecnico/SolicitudServicioTecnico.java
+++ dom/src/main/java/dom/SolicitudDeServicioTecnico/SolicitudServicioTecnico.java
@@ -5,10 +5,14 @@ package dom.SolicitudDeServicioTecnico;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import java.io.IOException;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 
@@ -17,9 +21,18 @@ import javax.jdo.annotations.VersionStrategy;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.MultiLine;
@@ -27,6 +40,7 @@ import org.apache.isis.applib.annotation.ObjectType;
 import org.apache.isis.applib.annotation.Optional;
 import org.apache.isis.applib.annotation.Title;
 import org.apache.isis.applib.value.Blob;
+import org.eclipse.jdt.core.dom.ThisExpression;
 
 
 
@@ -199,13 +213,39 @@ public class SolicitudServicioTecnico {
      * archivo incompleto para imprimir
      * @return Reporte a imprimir
      * @throws JRException 
+     * @throws FileNotFoundException 
      */
-	public Blob imprimir() throws JRException
+	public Blob imprimir() throws JRException, FileNotFoundException
 	{
 		
-
+		
+		/*JasperReport jr = JasperCompileManager.compileReport("reportes/solicitudAsistenciaTecnica.jrxml");
+		//para no compilarlo
+		//JasperReport jr = (JasperReport) JRLoader.loadObject("solicitudAsistenciaTecnica.jasper");
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("nombre",this.getNetbook().getModelo().toString());
+		JRExporter exporter = new JRPdfExporter();
+		JasperPrint jp = JasperFillManager.fillReport(jr, map);
+		exporter.setParameter(JRExporterParameter.JASPER_PRINT,jp); 
+		exporter.setParameter(JRExporterParameter.OUTPUT_FILE,new java.io.File("/tmp/solicitudAsistenciaTecnica.pdf"));
+		exporter.exportReport();*/
+		Object[] obj= new Object[1];
+		obj[0]="";
+		JRBeanArrayDataSource jrDataSource= new JRBeanArrayDataSource(obj);
+		File file = new File("reportes/solicitudAsistenciaTecnica.jrxml");
+		InputStream input = new FileInputStream(file);
+		JasperDesign jd = JRXmlLoader.load(input);
+		JasperReport reporte = JasperCompileManager.compileReport(jd);
+		JasperPrint print = JasperFillManager.fillReport(reporte, null,jrDataSource);
+		
+		JRExporter exporter = new JRPdfExporter();
+		exporter.setParameter(JRExporterParameter.JASPER_PRINT,print); 
+		exporter.setParameter(JRExporterParameter.OUTPUT_FILE,new java.io.File("/tmp/solicitudAsistenciaTecnica.pdf"));
+		exporter.exportReport();
+		
+		
         
-		File resume = new File("solicitudAsistenciaTecnica.pdf");
+		File resume = new File("/tmp/solicitudAsistenciaTecnica.pdf");
 		if (!(resume.exists()))
 		{
 		try {
@@ -228,6 +268,7 @@ public class SolicitudServicioTecnico {
 		Blob blob= new Blob("solicitudAsistenciaTecnica.pdf","application/pdf",fileContent);
 			
 		return blob;
+		
 	}
 	
 
