db0032b2f5149051bc0d7e3662c4f83135ccbf85
Matias Nahuel Heredia <matias@informaticos.com>
2014-07-05 23:53:10 -0300

    agregar configuracion email

--- dom/src/main/java/servicio/email/ConfiguracionMail.java
+++ dom/src/main/java/servicio/email/ConfiguracionMail.java
@@ -2,6 +2,8 @@ package servicio.email;
 
 import java.util.Properties;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class ConfiguracionMail {
     private String puerto;
     private String host;
@@ -12,40 +14,165 @@ public class ConfiguracionMail {
     private Properties props = new Properties();
 	
     
-
-	public String getPassword() {
-		return password;
-	}
-
-
+    
+    
+    
 	public String getPuerto() {
 		return puerto;
 	}
+
+
+
+
+
+
+
+	public void setPuerto(String puerto) {
+		this.puerto = puerto;
+	}
+
+
+
+
+
 
 
 	public String getHost() {
 		return host;
 	}
 
+
+
+
+
+
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+
+
+
+
+
+
 	public boolean isTls() {
 		return tls;
 	}
+
+
+
+
+
+
+
+	public void setTls(boolean tls) {
+		this.tls = tls;
+	}
+
+
+
+
+
+
 
 	public String getUsuario() {
 		return usuario;
 	}
 
+
+
+
+
+
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+
+
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+
+
+
 	public boolean isAuth() {
 		return auth;
 	}
 
+
+
+
+
+
+
+	public void setAuth(boolean auth) {
+		this.auth = auth;
+	}
+
+
+
+
+
+
+
+	public Properties getProps() {
+		return props;
+	}
+
+
+
+
+
+
+
+	public void setProps(Properties props) {
+		this.props = props;
+	}
+
+
+
+
+
+
+
 	public void mailFactory()
 	{
-        props.put("mail.smtp.host", this.host);
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/configuracionemail.xml");
+		ConfiguracionMail configuracionMail = (ConfiguracionMail) context.getBean("gmail");
+		this.host=configuracionMail.host;
+		this.auth=configuracionMail.auth;
+		this.password=configuracionMail.password;
+		this.props=configuracionMail.props;
+		this.puerto=configuracionMail.puerto;
+		this.tls=configuracionMail.tls;
+		this.usuario=configuracionMail.usuario;
+		context.close();
+		props.put("mail.smtp.host", this.host);
         props.setProperty("mail.smtp.starttls.enable", ""+this.tls);
         props.setProperty("mail.smtp.port", ""+this.puerto);
         props.setProperty("mail.smtp.user", this.usuario);
         props.setProperty("mail.smtp.auth", ""+this.auth);
-		
 	}
 }
--- dom/src/main/java/servicio/email/email.java
+++ dom/src/main/java/servicio/email/email.java
@@ -2,7 +2,11 @@ package servicio.email;
 
 public class email {
 
-	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("ok");
+	}
 	
 	
 }
