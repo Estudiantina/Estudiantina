a0e2d38542db41049e21cb4ed168bcdd2d49306a
Matias Nahuel Heredia <matias@informaticos.com>
2014-07-27 22:56:09 -0300

    eliminar version persona

--- dom/src/main/java/dom/Tecnico/Tecnico.java
+++ dom/src/main/java/dom/Tecnico/Tecnico.java
@@ -1,7 +1,6 @@
 package dom.Tecnico;
 
 import javax.jdo.annotations.IdentityType;
-import javax.jdo.annotations.VersionStrategy;
 
 import org.apache.isis.applib.annotation.Audited;
 import org.apache.isis.applib.annotation.AutoComplete;
@@ -11,9 +10,6 @@ import repo.Persona.RepositorioPersona;
 import dom.Persona.Persona;
 
 @javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE)
-@javax.jdo.annotations.Version(
-        strategy=VersionStrategy.VERSION_NUMBER, 
-        column="version")
 @javax.jdo.annotations.Queries({@javax.jdo.annotations.Query(name = "traerPorcuil", language = "JDOQL", value = "SELECT FROM dom.Docente.Docente WHERE cuil== :cuil"),
 	@javax.jdo.annotations.Query(name = "traerTodoDocente", language = "JDOQL", value = "SELECT FROM dom.Docente.Docente")})
 @AutoComplete(repository = RepositorioPersona.class, action = "autoComplete")
