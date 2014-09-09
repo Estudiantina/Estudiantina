bd69d162c210512b46637d28ab935ad8f3e52b16
Matias Nahuel Heredia <matias@informaticos.com>
2014-09-09 15:29:03 -0300

    generar hashCode y equals En Provincia

--- dom/src/main/java/dom/Localidad/Provincia.java
+++ dom/src/main/java/dom/Localidad/Provincia.java
@@ -46,4 +46,30 @@ public class Provincia {
 		return this.nombreProvincia;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((nombreProvincia == null) ? 0 : nombreProvincia.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Provincia other = (Provincia) obj;
+		if (nombreProvincia == null) {
+			if (other.nombreProvincia != null)
+				return false;
+		} else if (!nombreProvincia.equals(other.nombreProvincia))
+			return false;
+		return true;
+	}
+
 }
