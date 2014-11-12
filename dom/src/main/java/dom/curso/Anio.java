c2306b04fa8dbf0fb7dd515644d6b7045bc64b3f
Matias Nahuel Heredia <matias@informaticos.com>
2014-11-12 20:06:21 -0300

    agregar declaracion de licencia en donde no habia

--- dom/src/main/java/dom/curso/Anio.java
+++ dom/src/main/java/dom/curso/Anio.java
@@ -1,5 +1,16 @@
 package dom.curso;
-
+/*
+ *  
+ *
+ *  Copyright (C) 2014 Estudiantina, All Rights Reserved.
+ *  Autors:
+ *  Matias Nahuel Heredia
+ *  Jose Luis Troche
+ *  Andres Robobich
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
 public enum Anio {
 	
 	primero, segundo, tercero, cuarto, quinto, sexto
