8ac943d3ad0a7fd11ae90b38d3d47f6b29ef4c03
Matias Nahuel Heredia <matias@informaticos.com>
2015-01-24 17:14:33 -0300

    validar ingresar Alumno

--- dom/src/main/java/repo/persona/RepositorioPersona.java
+++ dom/src/main/java/repo/persona/RepositorioPersona.java
@@ -128,6 +128,40 @@ public class RepositorioPersona extends AbstractFactoryAndRepository {
 	return alumno;
 	}
 	
+    
+    public String validateIngresarDirectivo (
+			@Named("CUIL") final Long cuil,
+			@RegEx(validation = "[A-Za-z ]+") @Named("NOMBRE")final String nombre,
+			@RegEx(validation = "[A-Za-z]+") @Named("APELLIDO")final String apellido,
+			@RegEx(validation = "[0-9]+")  @org.apache.isis.applib.annotation.Optional @Named("TELEFONO CELULAR")final String telefonoCelular,
+			@RegEx(validation = "[0-9]+")  @org.apache.isis.applib.annotation.Optional  @Named("TELEFONO FIJO")final String telefinoFijo,
+			@RegEx(validation = "(\\w+\\-)*(\\w+\\.)*\\w+@(\\w+\\.)+[A-Za-z]+") @Named("CORREO ELECTRONICO")final String email,
+			@Named("DOMICILIO") String domicilio,
+			@Named("Altura") int alturaDomicilio,
+			@Optional @Named("Piso") String piso,
+			@Named("Cod Postal Ciudad")Localidad localidad,
+			@Named("FECHA NACIMIENTO")final Date fechaNacimiento,
+			@Named("SEXO") Sexo sexo
+			)
+	{
+    	Date fechaIngreso = new Date();
+		if(fechaNacimiento.compareTo(fechaIngreso) >0)
+		{
+			return "FECHA NACIMIENTO: debe ser menor a la fecha de ingreso";
+		}
+		else if (Character.isUpperCase(nombre.charAt(0))==false)
+		{
+			return "El Nombre debe empezar con mayuscula";
+		}else 
+		if (Character.isUpperCase(apellido.charAt(0))==false)
+		{
+			return "El Apellido debe empezar con mayuscula";
+		}
+		else
+		{
+        return null;
+		}
+	}
 	//*********************** VALIDACION ******************************************//
 	/**
 	 * metodo que valida los parametros cuando se ingresa un nueva alumno
