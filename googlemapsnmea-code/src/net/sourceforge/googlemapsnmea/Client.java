10089a9b048795ec280dedb328fda9dae40eae68
Matias Nahuel Heredia <matias@informaticos.com>
2015-01-30 02:43:29 -0300

    agregar  modificacion de migracion de Netbook

--- googlemapsnmea-code/src/net/sourceforge/googlemapsnmea/Client.java
+++
@@ -1,6 +0,0 @@
-package net.sourceforge.googlemapsnmea;
-
-public interface Client {
-	public void sendUpdate(float[] location) throws Exception;
-	public void close();
-}
--- googlemapsnmea-code/src/net/sourceforge/googlemapsnmea/Connection.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.googlemapsnmea;
-
-import java.io.*;
-
-public interface Connection{
-	public void connect(String portname) throws IOException;
-	public String readLine() throws IOException;
-	public boolean ready();
-}
